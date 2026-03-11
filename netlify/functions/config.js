079149a80e79494c22772a329f3a9c5999e528b7
La Bibi <contacto@labibi.es>
2026-03-11 11:02:41 +0100

    syscall

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,11 +1,24 @@
 // netlify/functions/config.js
 exports.handler = async function() {
+  // Configurar CORS
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Content-Type': 'application/json'
+  };
+
+  // Si es preflight OPTIONS
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers,
+      body: ''
+    };
+  }
+
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    },
+    headers,
     body: JSON.stringify({
       url: process.env.SUPABASE_URL || 'https://swsrywvjskhshlbtbzrx.supabase.co',
       anonKey: process.env.SUPABASE_ANON_KEY
